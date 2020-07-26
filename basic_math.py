8c5c29cc20b6d6a7b99d7bc20262f500ba377c47
drakiiii <drakonakisa@gmail.com>
2020-07-26 22:51:21 +0100

    Updating basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -1,6 +1,5 @@
-x = 100
-for y in range(1,20):
-    if y == x:
-        print(y, x)
-    else:
-        print(y, x)
+#!/usr/bin/env python3.8
+x = 0
+for x in range(0,100):
+    y = (x ** 2) - 1
+    print(x,y)
